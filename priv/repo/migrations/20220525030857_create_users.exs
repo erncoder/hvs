ae67d944d0531c9081f9443032572a8a65a1c598
Aaron Davis <aaron@plukkit.com>
2022-05-24 21:11:07 -0600

    add user context/schema

---
+++ priv/repo/migrations/20220525030857_create_users.exs
@@ -0,0 +1,14 @@
+defmodule Hvs.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      add :email, :string
+      add :mins_balance, :integer
+
+      timestamps()
+    end
+  end
+end
---
+++ test/support/fixtures/users_fixtures.ex
@@ -0,0 +1,23 @@
+defmodule Hvs.UsersFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Hvs.Users` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        email: "some email",
+        first_name: "some first_name",
+        last_name: "some last_name",
+        mins_balance: 42
+      })
+      |> Hvs.Users.create_user()
+
+    user
+  end
+end
